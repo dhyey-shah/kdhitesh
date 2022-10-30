27ab058ed730b46f14e0001ee155c6d37f03b5b7
Dhyey <dhyeyshah4@gmail.com>
2022-10-30 21:30:40 +0530

    #GH-5 add gallery view

--- src/components/masonry/Masonry.jsx
+++ src/components/masonry/Masonry.jsx
@@ -1,14 +1,26 @@
 import MasonryGrid from "./MasonryGrid";
 import MediaViewer from "./MediaViewer";
+import styled from "styled-components";
+
+const MoreLikeThis = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 24px 0;
+  font-weight: bolder;
+  font-size: large;
+`
 
 function Masonry(props) {
     return (
         <>
             {
                 props.imgId &&
-                <MediaViewer
-                    mediaObject={props.media[props.imgId]}
-                />
+                (
+                    <>
+                        <MediaViewer mediaObject={props.media[props.imgId]} />
+                        <MoreLikeThis>More Like This</MoreLikeThis>
+                    </>
+                )
             }
             <MasonryGrid
                 media={props.media}
